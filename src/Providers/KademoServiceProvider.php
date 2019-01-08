911a0e16e9acac240aa0ef004bc201fecd497855
Misha Yatsenko <1234asdfm@gmail.com>
2019-01-08 11:14:09 +0200

    init

---
+++ src/Providers/KademoServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kademo\Providers;
+
+use IO\Helper\TemplateContainer;
+use IO\Helper\ResourceContainer;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
+
+/**
+ * Class KademoServiceProvider
+ * @package Kademo\Providers
+ */
+class KademoServiceProvider extends ServiceProvider
+{
+    const PRIORITY = 98;
+
+    public function register()
+    {
+
+    }
+
+    public function boot(Twig $twig, Dispatcher $dispatcher)
+    {
+        $dispatcher->listen('IO.tpl.item', function (TemplateContainer $container)
+        {
+            $container->setTemplate('Kademo::Item.SingleItemWrapper');
+            return false;
+        }, self::PRIORITY);
+    }
+}
+
